4d3236b75d504859200d06882008cfe41aeb099d
NamikazeAsh <ashwinsatish0@gmail.com>
2023-12-06 11:31:07 +0530

    success messages

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import TodoItem
 from .serializers import TodoItemSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class TodoItemCreateView(generics.CreateAPIView):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({"message": "TodoItem created successfully"}, status=status.HTTP_201_CREATED)
 
 class TodoItemReadOneView(generics.RetrieveAPIView):
     queryset = TodoItem.objects.all()
@@ -19,6 +26,15 @@ class TodoItemUpdateView(generics.UpdateAPIView):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
 
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response({"message": "TodoItem updated successfully"}, status=status.HTTP_200_OK)
+
 class TodoItemDeleteView(generics.DestroyAPIView):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "TodoItem deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
