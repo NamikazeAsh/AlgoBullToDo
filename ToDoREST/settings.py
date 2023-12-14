28a5b1fd5866a1a0fff29be6b2f4d68399c5417f
NamikazeAsh <ashwinsatish0@gmail.com>
2023-12-14 12:59:43 +0530

    added :8000 to trusted origins

--- ToDoREST/settings.py
+++ ToDoREST/settings.py
@@ -28,12 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'https://algobulltodo-production.up.railway.app/',
     'https://127.0.0.1',
+    'https://127.0.0.1:8000',
     'https://algobulltodo-production.up.railway.app'
 ]
 
 CSRF_TRUSTED_ORIGINS=[
     'https://algobulltodo-production.up.railway.app/',
     'https://127.0.0.1',
+    'https://127.0.0.1:8000',
     'https://algobulltodo-production.up.railway.app'
 ]
 
