148f1237a11d8de3962f3d476c7bc8b4d389d21c
NamikazeAsh <ashwinsatish0@gmail.com>
2023-12-14 13:05:50 +0530

    allowed host header

--- ToDoREST/settings.py
+++ ToDoREST/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'https://algobulltodo-production.up.railway.app/',
     '127.0.0.1',
-    'https://algobulltodo-production.up.railway.app'
+    'https://algobulltodo-production.up.railway.app',
+    'algobulltodo-production.up.railway.app'
 ]
 
 CSRF_TRUSTED_ORIGINS=[
